f5750b9a6af15346c9c1ac754baa4e193e57ed7f
Nikita Yeldynov <yeldynov@gmail.com>
2024-10-09 14:07:59 +0300

    + clerk auth

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
+import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
-import { role } from '@/lib/data'
 
 const menuItems = [
   {
@@ -117,7 +117,10 @@ const menuItems = [
   },
 ]
 
-const Menu = () => {
+const Menu = async () => {
+  const user = await currentUser()
+  const role = user?.publicMetadata.role as string
+
   return (
     <div className='mt-4 text-sm'>
       {menuItems.map((i) => (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser()
+
   return (
     <div className='flex items-center justify-between p-4'>
       {/* Search bar */}
@@ -26,15 +30,18 @@ const Navbar = () => {
         </div>
         <div className='flex flex-col'>
           <span className='text-xs font-medium leading-3'>Johnny Walker</span>
-          <span className='text-[10px] text-gray-500 text-right'>Admin</span>
+          <span className='text-[10px] text-gray-500 text-right'>
+            {user?.publicMetadata.role as string}
+          </span>
         </div>
-        <Image
+        {/* <Image
           src='/avatar.png'
           alt=''
           width={36}
           height={36}
           className='rounded-full'
-        />
+        /> */}
+        <UserButton />
       </div>
     </div>
   )
